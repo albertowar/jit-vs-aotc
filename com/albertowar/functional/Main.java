5d4c9886faf863b9f28a921cbce4abdcc01b045e
Alberto Guerra Gonzalez <aguerra@riotgames.com>
2021-03-07 19:35:51 +0000

    Skipping redundant iteration

--- com/albertowar/functional/Main.java
+++ com/albertowar/functional/Main.java
@@ -2,7 +2,7 @@ package com.albertowar.functional;
 
 
 public class Main {
-    private static final int[] input = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    private static final int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator(input);
